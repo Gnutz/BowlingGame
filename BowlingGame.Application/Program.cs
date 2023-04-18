7cf2826a3ff1213bd1715b83dfcb5e14dc4c0c53
KasperGnutzmann <kga@live.dk>
2023-04-18 23:38:19 +0200

    added choice of simulation type to client application

--- BowlingGame.Application/Program.cs
+++ BowlingGame.Application/Program.cs
@@ -8,14 +8,35 @@ namespace BowlingGame.Application
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please select how you wish to generate rolls for this bowling game simulation:");
+            Console.WriteLine("Please enter '1' to enter the rolls yourself");
+            Console.WriteLine("Please enter '2' to get automated rolls");
+            var choice = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
             var scorer = new TraditionalScoreStrategy();
-            var rollProvider = new ManualRollProvider();
+            IRollProvider rollProvider = null;
+            int delay = 2000;
+
+            switch (choice)
+            {
+                case '1':
+                    rollProvider = new ManualRollProvider();
+                    delay = 100;
+                    break;
+                case '2':
+                    rollProvider = new RandomRollProvider();
+                    break;
+                default:
+                    break;
+            }
             var game = new Game.BowlingGame(scorer, rollProvider);
             var presenter = new BowlingScorePresenter();
             
             presenter.DisplayGame(game.GetFrames());
             while (!game.IsGameCompleted())
             { 
+                Thread.Sleep(delay);
                 game.RollBowlingBall(); 
                 presenter.DisplayGame(game.GetFrames());
             }
